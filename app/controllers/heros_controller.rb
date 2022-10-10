1b42b95c2be0895ce3049893c44ae5b5c06867eb
ndurivin <ndurivo@gmail.com>
2022-10-10 22:33:20 +0300

    Adds custome serializer to Hero controller

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -8,7 +8,7 @@ class HerosController < ApplicationController
     def show 
         hero = Hero.find_by(id: params[:id])
         if hero 
-            render json: hero, status: :ok
+            render json: hero, serializer: HeroAndPowerSerializer, status: :ok
         else 
             render json: { error: "Hero not found" }, status: :not_found
         end
--- app/serializers/hero_and_power_serializer.rb
+++ app/serializers/hero_and_power_serializer.rb
@@ -1,3 +1,6 @@
 class HeroAndPowerSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :super_name
+
+  has_many :powers, through: :hero_powers 
+
 end
