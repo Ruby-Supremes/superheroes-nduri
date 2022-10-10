ba4516ce3798ddf14bb43394aab838baf8102fd9
ndurivin <ndurivo@gmail.com>
2022-10-10 22:59:15 +0300

    Adds update method in the power controller

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -4,4 +4,31 @@ class PowersController < ApplicationController
         powers = Power.all 
         render json: powers, status: :ok
     end
+
+    def show
+        power = Power.find_by(id: params[:id])
+        if power 
+            render json: power, status: :ok
+        else 
+            render jason: { error: "Power not found" }, status: :not_found
+        end
+    end
+
+    def update
+        power = Power.find_by(id: params[:id])
+        if power
+          power.update(power_params)
+          render json: power, status: :created
+        else
+          render json: { error: "Power not found" }, status: :not_found
+        end
+      end
+
+
+    private
+
+    def power_params
+        params.require(:power).permit(:description)
+    end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  resources :hero_powers
-  resources :powers, only: [:index]
+  resources :hero_powers, only: [:create]
+  resources :powers, only: [:index, :show, :update]
   resources :heros, only: [:index, :show]
 end
